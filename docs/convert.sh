6540dd45582a82267b1ffd318f4146324d40a6fe
Tim Bishop <tim@bishnet.net>
2021-02-07 23:16:43 +0000

    WIP.

--- docs/convert.sh
+++ docs/convert.sh
@@ -19,8 +19,8 @@ for i in "$@"; do
 		docbook2x-man -N --no-links --string-param header-3="`git log -1 --format='%ad' --date=short $i`" --to-stdout $i | pandoc -f man -t asciidoctor >> $i.adoc
 	fi
 
-	docbook2x-man -N --no-links --string-param header-3="`git log -1 --format='%ad' --date=short $i`" --to-stdout $i | man -l - | col -b > $i.original
-	/home/cur/tdb/.gem/ruby/2.5.0/bin/asciidoctor -a docdate="`git log -1 --format='%ad' --date=short $i`" -b manpage -o - $i.adoc | man -l - | col -b > $i.new
+	docbook2x-man -N --no-links --string-param header-3="`git log -1 --format='%ad' --date=short $i`" --to-stdout $i | man --nj --nh -l - | col -b > $i.original
+	/home/cur/tdb/.gem/ruby/2.5.0/bin/asciidoctor -a docdate="`git log -1 --format='%ad' --date=short $i`" -b manpage -o - $i.adoc | man --nj --nh -l - | col -b > $i.new
 
 	diff -u $i.original $i.new
 done
